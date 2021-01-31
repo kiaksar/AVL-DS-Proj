e29b92d6a4e95514ce085d4b33e0bc41ab9eccad
kiaksar <kiaksar.shirvani.m@gmail.com>
2021-01-31 23:47:49 +0330

    making the node class

---
+++ AVL/AVL/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVL
+{
+    class Node
+    {
+        public string value;
+        public string meaning;
+        public int height;
+        public Node right;
+        public Node left;
+        public string idLine;
+        public string idEnviroment;
+        public Node(string value, string meaning, string idEnviroment)
+        {
+            this.value = value;
+            this.meaning = meaning;
+            this.idEnviroment = idEnviroment;
+            height = 1;
+        }
+    }
+}
