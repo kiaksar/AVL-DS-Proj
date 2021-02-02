3777144922890c53ac3757750978c5ccbaa8a91a
kiaksar <kiaksar.shirvani.m@gmail.com>
2021-02-02 23:13:52 +0330

    completing AVL class

--- AVL/AVL/AVLTree.cs
+++ AVL/AVL/AVLTree.cs
@@ -6,17 +6,16 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
-
-namespace AVL
+namespace AVL_Tree
 {
-    class AVLTree
+    public class AVLTree
     {
         public Node root;
         public string inOrderResult;
 
         // A utility function to get height of the tree  
         public Dictionary<string, Lingkaran> lingkaran = new Dictionary<string, Lingkaran>();
-        public Dictionary<string, Garis> garis= new Dictionary<string, Garis>();
+        public Dictionary<string, Garis> garis = new Dictionary<string, Garis>();
 
         Form1 form;
         int ctrGaris = 1;
@@ -85,6 +84,8 @@ namespace AVL
             // Return new root  
             return y;
         }
+
+        // Get Balance factor of node N  
         public void delay()
         {
             form.pictureBox1.Invalidate();
@@ -92,7 +93,6 @@ namespace AVL
             Thread.Sleep(300);
 
         }
-        // Get Balance factor of node N 
         int getBalance(Node N)
         {
             if (N == null)
@@ -191,5 +191,256 @@ namespace AVL
             return node;
         }
 
+        /* Given a non-empty binary search tree, return the  
+        node with minimum key value found in that tree.  
+        Note that the entire tree does not need to be  
+        searched. */
+        Node minValueNode(Node node)
+        {
+            Node current = node;
+
+            /* loop down to find the leftmost leaf */
+            while (current.left != null)
+                current = current.left;
+
+            return current;
+        }
+
+        public Node deleteNode(Node root, int key)
+        {
+            // STEP 1: PERFORM STANDARD BST DELETE  
+            if (root == null)
+            {
+                form.Status.Text = "Deleted key not found in tree";
+                delay();
+                return root;
+            }
+
+            // If the key to be deleted is smaller than  
+            // the root's key, then it lies in left subtree  
+            if (key < root.key)
+            {
+                form.Status.Text = key + " is lesser than " + root.key;
+                lingkaran[root.idLingkaran].brush = new SolidBrush(Color.Yellow);
+                delay();
+                form.Status.Text = " Current go to left";
+                lingkaran[root.idLingkaran].brush = new SolidBrush(Color.Black);
+                delay();
+
+                root.left = deleteNode(root.left, key);
+            }
+
+            // If the key to be deleted is greater than the  
+            // root's key, then it lies in right subtree  
+            else if (key > root.key)
+            {
+                form.Status.Text = key + " is greater than " + root.key;
+                lingkaran[root.idLingkaran].brush = new SolidBrush(Color.Yellow);
+                delay();
+                form.Status.Text = " Current go to right";
+                lingkaran[root.idLingkaran].brush = new SolidBrush(Color.Black);
+                delay();
+
+                root.right = deleteNode(root.right, key);
+            }
+
+            // if key is same as root's key, then this is the node  
+            // to be deleted  
+            else
+            {
+                form.Status.Text = "Node is found";
+                lingkaran[root.idLingkaran].brush = new SolidBrush(Color.Yellow);
+                delay();
+
+                // node with only one child or no child  
+                if ((root.left == null) || (root.right == null))
+                {
+                    Node temp = null;
+                    if (temp == root.left)
+                        temp = root.right;
+                    else
+                        temp = root.left;
+
+                    // No child case  
+                    if (temp == null)
+                    {
+                        temp = root;
+                        root = null;
+                    }
+                    else // One child case  
+                        root = temp; // Copy the contents of  
+                                     // the non-empty child  
+                }
+                else
+                {
+
+                    // node with two children: Get the inorder  
+                    // successor (smallest in the right subtree)  
+                    Node temp = minValueNode(root.right);
+
+                    // Copy the inorder successor's data to this node  
+                    root.key = temp.key;
+
+                    // Delete the inorder successor  
+                    root.right = deleteNode(root.right, temp.key);
+                }
+
+            }
+
+            // If the tree had only one node then return  
+            if (root == null)
+                return root;
+
+            // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE  
+            root.height = max(height(root.left), height(root.right)) + 1;
+
+            // STEP 3: GET THE BALANCE FACTOR OF THIS NODE (to check whether  
+            // this node became unbalanced)  
+            int balance = getBalance(root);
+
+            // If this node becomes unbalanced, then there are 4 cases  
+            // Left Left Case  
+            if (balance > 1 && getBalance(root.left) >= 0)
+            {
+                form.Status.Text = "Right Rotate on " + root.key;
+                delay();
+                return rightRotate(root);
+            }
+            // Left Right Case  
+            if (balance > 1 && getBalance(root.left) < 0)
+            {
+
+                form.Status.Text = "Double Right Rotate on " + root.key;
+                delay();
+
+                root.left = leftRotate(root.left);
+                return rightRotate(root);
+            }
+
+            // Right Right Case  
+            if (balance < -1 && getBalance(root.right) <= 0)
+            {
+                form.Status.Text = "Left Rotate on " + root.key;
+                delay();
+                return leftRotate(root);
+            }
+
+            // Right Left Case  
+            if (balance < -1 && getBalance(root.right) > 0)
+            {
+                form.Status.Text = "Double Rotate on " + root.key;
+                delay();
+
+                root.right = rightRotate(root.right);
+                return leftRotate(root);
+            }
+
+            return root;
+        }
+
+        // A utility function to print preorder traversal of  
+        // the tree. The function also prints height of every  
+        // node  
+
+        public void preOrder(Node node)
+        {
+            if (node != null)
+            {
+                Console.WriteLine(node.key + " ");
+                preOrder(node.left);
+                preOrder(node.right);
+            }
+        }
+        public void find(int value)
+        {
+            if (root == null) return;
+            Node current = root;
+            while (current != null)
+            {
+                if (value < current.key)
+                {
+                    form.Status.Text = value + " is lesser than " + current.key;
+                    lingkaran[current.idLingkaran].brush = new SolidBrush(Color.Yellow);
+                    delay();
+                    form.Status.Text = " Current go to left";
+                    lingkaran[current.idLingkaran].brush = new SolidBrush(Color.Black);
+                    delay();
+                    current = current.left;
+                }
+                else if (value > current.key)
+                {
+                    form.Status.Text = value + " is greater than " + current.key;
+                    lingkaran[current.idLingkaran].brush = new SolidBrush(Color.Yellow);
+                    delay();
+                    form.Status.Text = " Current go to right";
+                    lingkaran[current.idLingkaran].brush = new SolidBrush(Color.Black);
+                    current = current.right;
+                    delay();
+                }
+                else
+                {
+                    form.Status.Text = value + " exist in tree";
+                    lingkaran[current.idLingkaran].brush = new SolidBrush(Color.Yellow);
+                    delay();
+                    lingkaran[current.idLingkaran].brush = new SolidBrush(Color.Black);
+                    form.mbox(value + " has been found in tree");
+                    delay();
+                    return;
+                }
+
+            }
+            form.Status.Text = value + "Value not found";
+            form.mbox(value + " doesn't exist in tree");
+            delay();
+
+        }
+        public void inOrder(Node root)
+        {
+            if (root == null) return;
+            inOrder(root.left);
+            lingkaran[root.idLingkaran].brush = new SolidBrush(Color.Yellow);
+            delay();
+            inOrderResult += root.key + " ";
+            inOrder(root.right);
+
+        }
+
+        public void inOrderHelper()
+        {
+            lingkaran.Clear();
+            garis.Clear();
+            ctrLingkaran = 1;
+            ctrGaris = 1;
+            inOrderClear(ref this.root, 1, 1, 50);
+        }
+        public void inOrderClear(ref Node root, int kali, int bagi, int tinggi)
+        {
+            if (root == null) return;
+            inOrderClear(ref root.left, (kali * 2) - 1, bagi * 2, tinggi + 100);
+
+            lingkaran.Add("lingkaran" + ctrLingkaran, new Lingkaran(484 / bagi * kali + 1, tinggi, root.key.ToString()));
+            root.idLingkaran = "lingkaran" + ctrLingkaran;
+            ctrLingkaran++;
+            form.pictureBox1.Invalidate();
+            Application.DoEvents();
+            if (root.left != null)
+            {
+                garis.Add("garis" + ctrGaris, new Garis((484 / bagi * kali + 1) + 25, tinggi + 30, (484 / (bagi * 2)) * ((kali * 2) - 1) + 25, tinggi + 100));
+                ctrGaris++;
+                form.pictureBox1.Invalidate();
+            }
+
+            if (root.right != null)
+            {
+                garis.Add("garis" + ctrGaris, new Garis((484 / bagi * kali + 1) + 25, tinggi + 30, (484 / (bagi * 2)) * ((kali * 2) + 1) + 25, tinggi + 100));
+                ctrGaris++;
+                form.pictureBox1.Invalidate();
+            }
+            inOrderClear(ref root.right, (kali * 2) + 1, bagi * 2, tinggi + 100);
+        }
+
+
+
     }
+
 }
--- AVL/AVL/Form1.Designer.cs
+++ AVL/AVL/Form1.Designer.cs
@@ -37,22 +37,24 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.Status = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.meaningBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // valueBox
             // 
-            this.valueBox.Location = new System.Drawing.Point(41, 30);
+            this.valueBox.Location = new System.Drawing.Point(55, 37);
+            this.valueBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.valueBox.Name = "valueBox";
-            this.valueBox.Size = new System.Drawing.Size(124, 20);
+            this.valueBox.Size = new System.Drawing.Size(164, 22);
             this.valueBox.TabIndex = 0;
             // 
             // InsertBtn
             // 
-            this.InsertBtn.Location = new System.Drawing.Point(364, 30);
+            this.InsertBtn.Location = new System.Drawing.Point(485, 37);
+            this.InsertBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.InsertBtn.Name = "InsertBtn";
-            this.InsertBtn.Size = new System.Drawing.Size(75, 23);
+            this.InsertBtn.Size = new System.Drawing.Size(100, 28);
             this.InsertBtn.TabIndex = 1;
             this.InsertBtn.Text = "Insert";
             this.InsertBtn.UseVisualStyleBackColor = true;
@@ -60,9 +62,10 @@
             // 
             // deleteBtn
             // 
-            this.deleteBtn.Location = new System.Drawing.Point(456, 30);
+            this.deleteBtn.Location = new System.Drawing.Point(608, 37);
+            this.deleteBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.deleteBtn.Name = "deleteBtn";
-            this.deleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteBtn.Size = new System.Drawing.Size(100, 28);
             this.deleteBtn.TabIndex = 2;
             this.deleteBtn.Text = "Delete";
             this.deleteBtn.UseVisualStyleBackColor = true;
@@ -70,9 +73,10 @@
             // 
             // Find
             // 
-            this.Find.Location = new System.Drawing.Point(553, 30);
+            this.Find.Location = new System.Drawing.Point(737, 37);
+            this.Find.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Find.Name = "Find";
-            this.Find.Size = new System.Drawing.Size(75, 23);
+            this.Find.Size = new System.Drawing.Size(100, 28);
             this.Find.TabIndex = 3;
             this.Find.Text = "Find";
             this.Find.UseVisualStyleBackColor = true;
@@ -80,9 +84,10 @@
             // 
             // InOrder
             // 
-            this.InOrder.Location = new System.Drawing.Point(655, 30);
+            this.InOrder.Location = new System.Drawing.Point(873, 37);
+            this.InOrder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.InOrder.Name = "InOrder";
-            this.InOrder.Size = new System.Drawing.Size(75, 23);
+            this.InOrder.Size = new System.Drawing.Size(100, 28);
             this.InOrder.TabIndex = 4;
             this.InOrder.Text = "InOrder";
             this.InOrder.UseVisualStyleBackColor = true;
@@ -91,9 +96,10 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 73);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 90);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1366, 640);
+            this.pictureBox1.Size = new System.Drawing.Size(1821, 788);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -103,9 +109,10 @@
             // 
             this.Status.AutoSize = true;
             this.Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Status.Location = new System.Drawing.Point(706, 632);
+            this.Status.Location = new System.Drawing.Point(941, 778);
+            this.Status.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Status.Name = "Status";
-            this.Status.Size = new System.Drawing.Size(66, 24);
+            this.Status.Size = new System.Drawing.Size(85, 29);
             this.Status.TabIndex = 6;
             this.Status.Text = "Status";
             this.Status.TextChanged += new System.EventHandler(this.Status_TextChanged);
@@ -116,20 +123,21 @@
             this.timer1.Interval = 7000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // textBox1
+            // meaningBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(171, 30);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(124, 20);
-            this.textBox1.TabIndex = 7;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.meaningBox.Location = new System.Drawing.Point(228, 37);
+            this.meaningBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.meaningBox.Name = "meaningBox";
+            this.meaningBox.Size = new System.Drawing.Size(164, 22);
+            this.meaningBox.TabIndex = 7;
+            this.meaningBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1350, 681);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1800, 838);
+            this.Controls.Add(this.meaningBox);
             this.Controls.Add(this.Status);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.InOrder);
@@ -137,6 +145,7 @@
             this.Controls.Add(this.deleteBtn);
             this.Controls.Add(this.InsertBtn);
             this.Controls.Add(this.valueBox);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -156,7 +165,7 @@
         public System.Windows.Forms.PictureBox pictureBox1;
         public System.Windows.Forms.Label Status;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox meaningBox;
     }
 }
 
